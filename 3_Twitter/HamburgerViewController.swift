b9f76d32cb09789836be5f05f805cd574be097ad
Nathan Shayefar <nshayefar@twitter.com>
2015-03-01 22:57:58 -0800

    Start adding viewController management to HamburgerViewController.

--- 3_Twitter/HamburgerViewController.swift
+++ 3_Twitter/HamburgerViewController.swift
@@ -6,18 +6,61 @@
 //  Copyright (c) 2015 Nathan Shayefar. All rights reserved.
 //
 
-class HamburgerViewController: UIViewController {
+class HamburgerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var tableView: UITableView!
     
     private var originalContainerViewCenter: CGPoint?
     private var containerViewMinRightCenter: CGPoint?
     private var containerViewMaxRightCenter: CGPoint?
     
+    private var viewControllers: [UIViewController]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Storyboarding
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let profileViewController = storyboard.instantiateViewControllerWithIdentifier("ProfileViewController") as! ProfileViewController
+        let homeTimelineViewController = storyboard.instantiateViewControllerWithIdentifier("HomeTimelineViewController") as!HomeTimelineViewController
+        let mentionsViewController = storyboard.instantiateViewControllerWithIdentifier("TweetDetailViewController") as! TweetDetailViewController
+        viewControllers = [profileViewController, homeTimelineViewController, mentionsViewController]
+        self.activeViewController = viewControllers?.first
+        
+        
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
 
         self.containerViewMinRightCenter = CGPoint(x: -containerView.bounds.width, y: view.center.y)
         self.containerViewMaxRightCenter = CGPoint(x: containerView.bounds.width / 2, y: view.center.y)
+        
+        self.containerView.center = self.containerViewMinRightCenter!
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    var activeViewController: UIViewController? {
+        didSet(previousViewController) {
+            if let oldViewController = previousViewController {
+                oldViewController.willMoveToParentViewController(nil)
+                oldViewController.view.removeFromSuperview()
+                oldViewController.removeFromParentViewController()
+            }
+            if let newViewController = activeViewController {
+                self.addChildViewController(newViewController)
+                self.view.bounds = self.containerView.bounds
+                self.view.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+                self.containerView.addSubview(newViewController.view)
+                newViewController.didMoveToParentViewController(self)
+            }
+        }
     }
 
     @IBAction func onPanContainerView(sender: UIPanGestureRecognizer) {
